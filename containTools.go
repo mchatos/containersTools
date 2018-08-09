440ab3d682a593be826f27ba7369de288c51e9b5
mchatos <37662107+mchatos@users.noreply.github.com>
2018-08-09 11:48:37 +0200

    GetOSinfos

--- containTools.go
+++ containTools.go
@@ -1,4 +1,3 @@
-//containtools.go
 package main
 
 import (
@@ -10,39 +9,39 @@ import (
 )
 
 // Fonction sleep
-func fSleep(i int) {
+func fSleep(i int){
 
-	fmt.Println("So tired... i will sleep for ", i, " seconds...")
-	for j := 1; j <= i; j++ {
-		time.Sleep(1 * time.Second)
-	}
+  fmt.Println("So tired... i will sleep for ", i, " seconds...")
+  for j := 1; j <= i; j++ {
+      time.Sleep(1 * time.Second)
+  }
 }
 
 func fWaiTime() int {
 
-	s1 := rand.NewSource(time.Now().UnixNano())
-	r1 := rand.New(s1)
-	var w = (r1.Intn(100))
+  s1 := rand.NewSource(time.Now().UnixNano())
+  r1 := rand.New(s1)
+  var w = (r1.Intn(100))
 
-	return w
+  return w
 }
 
 func main() {
-	
+
 	siesta := 0
 	loop := 0
 	avg := 0
 	var tab [100]int
-	
+
 	fmt.Println("")
-	osinfos.OSinfos()
+	osinfos.GetOSinfos()
 
 	// infinite loop
 	for {
 		sleepTime := fWaiTime()
 		tab[loop] = sleepTime
 		loop++
-		
+
 		if ( sleepTime >= 90 || loop == 100 ) {
 			fmt.Printf("Siesta finished after %v Seconds, with %v micro sleeps generated \n", (siesta), (loop) )
 			avg=((siesta)/(loop-1))
