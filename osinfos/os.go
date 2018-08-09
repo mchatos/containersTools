dce167e00bea57aebad7a0e84d9b20c5a65ca40c
mchatos <37662107+mchatos@users.noreply.github.com>
2018-08-09 17:39:35 +0200

    update OSDetection

--- osinfos/os.go
+++ osinfos/os.go
@@ -5,20 +5,36 @@ import	(
 	"os"
 	)
 
-	// Os Functions
-	func GetOSinfos(){
 
-	  fmt.Println("OS Host Detection and informations : ")
-	  hostname, hostnameError := os.Hostname()
-	  osWin, osBool := os.LookupEnv("OS")
+func OSDetection() (string) {
+
+	fmt.Println("OS Host Detection... ")
+
+  var operatingsystem string
+
+	_, osBool := os.LookupEnv("OS")
+
+		  if osBool == false {
+				operatingsystem = "unix"
+				} else {
+				operatingsystem = "win"
+				}
+
+	return operatingsystem
+
+}
+
+// Os Functions
+func GetOSinfos(){
+
+		hostname, hostnameError := os.Hostname()
 
 	  fmt.Printf("PID : %v, PPID : %v\n", os.Getpid(), os.Getppid())
 
 	  if hostnameError != nil {panic(hostnameError)}
 	  fmt.Println("Hostname : ", hostname)
 
-	  if osBool == false {
-	      fmt.Println("OS Linux")
+		  if OSDetection() == "unix" {
 	      fmt.Printf("User %s lives in %s with UID %v and GID %v.\n", os.Getenv("USER"), os.Getenv("HOME"), os.Geteuid(), os.Getgid())
 	      fmt.Printf("Hostname %s.\n", os.Getenv("HOSTNAME"))
 	      // if _, err := os.Stat("/usr/sbin/ip"); err == nil {
@@ -27,7 +43,6 @@ import	(
 	      // }
 	      // fmt.Printf
 	    } else {
-	      fmt.Println("OS Windows : ", osWin)
 	      fmt.Printf("User %s lives in %s.\n", os.Getenv("USERNAME"), os.Getenv("HOMEPATH"))
 	  }
 	 fmt.Println(" ")
